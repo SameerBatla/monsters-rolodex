b5c21f01f7944380b92637165fd5a9e990a4a540
SameerBatla <sambatla456@gmail.com>
2019-12-08 21:46:40 +0530

    Updates

---
+++ precache-manifest.8924954836c4ea3c1e41d505b88c0d70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6092434a4e4c5ae6138770f5dff4dd92",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6de00e999c00676c8b7",
+    "url": "/monsters-rolodex/static/css/main.ca21ee46.chunk.css"
+  },
+  {
+    "revision": "f13c480240d50160f0b4",
+    "url": "/monsters-rolodex/static/js/2.3b4649f7.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/monsters-rolodex/static/js/2.3b4649f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6de00e999c00676c8b7",
+    "url": "/monsters-rolodex/static/js/main.efc62989.chunk.js"
+  },
+  {
+    "revision": "8f564ee1c346f305c76d",
+    "url": "/monsters-rolodex/static/js/runtime-main.17d07582.js"
+  }
+]);
