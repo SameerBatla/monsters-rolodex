7e1e98b83c0f95175b0f572977fac43b159a1dcd
SameerBatla <sambatla456@gmail.com>
2019-12-08 21:59:49 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Cardlist from "./components/card-list/Cardlist";
+import Searchbox from "./components/search-box/Searchbox";
+import "../src/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response =>
+        this.setState({
+          monsters: response.data
+        })
+      )
+      .catch(error => console.log(error));
+  }
+  handleChange = e => {
+    this.setState({
+      searchField: e.target.value
+    });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div>
+        {/* <input
+          type="search"
+          placeholder="Search here"
+          onChange={this.handleChange}
+        /> */}
+        <h1>MONSTERS ROLODEX</h1>
+        <Searchbox placeholder="Search Here" handleChange={this.handleChange} />
+        <Cardlist monsters={filteredMonsters}></Cardlist>
+      </div>
+    );
+  }
 }
 
 export default App;
